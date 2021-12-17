51c9071829faffff66c66da19a642dc7ec1233ba
Corentin CLOUET <clouetcorentin@gmail.com>
2021-12-17 19:52:23 +0100

    feat: save form data in preferences

--- app/src/main/java/com/coreclouet/getscan/ui/activity/MainActivity.kt
+++ app/src/main/java/com/coreclouet/getscan/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.coreclouet.getscan.ui.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.View
@@ -23,9 +24,21 @@ class MainActivity : AppCompatActivity() {
         binding.btnDownloadScan.setOnClickListener {
             downloadManga()
         }
+        initData()
         initObservers()
     }
 
+    /**
+     * Save form in preferences
+     */
+    override fun onPause() {
+        super.onPause()
+        savePreferences()
+    }
+
+    /**
+     * Init infos and loading observers
+     */
     private fun initObservers() {
         viewModel.infos.observe(this, { infos ->
             binding.tvInfoDownload.text = infos
@@ -36,6 +49,30 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    /**
+     * Init data with preferences
+     */
+    private fun initData() {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        binding.edBaseUrl.append(sharedPref.getString(getString(R.string.url_key), ""))
+        binding.edStartChapter.append(
+            sharedPref.getInt(
+                getString(R.string.first_chapter_key),
+                1
+            ).toString()
+        )
+        binding.edLastChapter.append(
+            sharedPref.getInt(
+                getString(R.string.last_chapter_key),
+                1
+            ).toString()
+        )
+        binding.edMangaName.append(sharedPref.getString(getString(R.string.manga_name_key), ""))
+    }
+
+    /**
+     * Check data and download manga
+     */
     private fun downloadManga() {
         if (viewModel.checkData(
                 binding.edBaseUrl.text.toString(),
@@ -48,6 +85,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Manage views states while loading
+     */
     private fun manageLoading(isLoading: Boolean) {
         if (isLoading) {
             binding.pbDownload.visibility = View.VISIBLE
@@ -60,6 +100,30 @@ class MainActivity : AppCompatActivity() {
         binding.edLastChapter.isEnabled = !isLoading
         binding.edMangaName.isEnabled = !isLoading
         binding.btnDownloadScan.isEnabled = !isLoading
+    }
+
+    /**
+     * Save form in preferences
+     */
+    private fun savePreferences() {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            if (binding.edBaseUrl.text.toString()
+                    .isNotEmpty()
+            ) putString(getString(R.string.url_key), binding.edBaseUrl.text.toString())
+            if (binding.edStartChapter.text.toString().isNotEmpty()) putInt(
+                getString(R.string.first_chapter_key),
+                binding.edStartChapter.text.toString().toInt()
+            )
+            if (binding.edLastChapter.text.toString().isNotEmpty()) putInt(
+                getString(R.string.last_chapter_key),
+                binding.edLastChapter.text.toString().toInt()
+            )
+            if (binding.edMangaName.text.toString()
+                    .isNotEmpty()
+            ) putString(getString(R.string.manga_name_key), binding.edMangaName.text.toString())
+            apply()
+        }
     }
 
 }
